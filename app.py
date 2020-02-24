873e73895570f16f8e47a399f33c48a1224ae120
WhiteJaeger <logerk3@gmail.com>
2020-02-24 14:43:48 +0300

    add pos tagger

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import secrets
-from flask import Flask, render_template, redirect, url_for, request, json
+from flask import render_template, redirect, url_for, request, json
 from forms import InputForm
-from os import getenv, path, remove
+from os import path, remove
 from waitress import serve
 from constants import METRICS_MAP, METRICS_FUNCTIONS
+from NLP.pos import features
 # from NLP.text_utils import prepare_text
+from . import create_app
+
+app, crf = create_app()
 
 
-app = Flask(__name__)
-app.secret_key = getenv('SECRET_KEY', secrets.token_urlsafe())
-
-
+# Metrics part
 @app.route('/')
 def hello_world():
     return render_template('home.html', title='Home Page')
@@ -19,7 +20,7 @@ def hello_world():
 @app.route('/metrics-sentence-level')
 def sl_metrics():
     form = InputForm()
-    output = read_metric_file()
+    output = read_temp_file()
 
     return render_template('metrics.html',
                            form=form,
@@ -31,7 +32,6 @@ def sl_metrics():
 
 @app.route('/api/handle-input', methods=['POST'])
 def process_input():
-
     metric = request.form.get('metric')
 
     if metric == 'rouge' or metric == 'meteor' or metric == 'chrf':
@@ -47,12 +47,43 @@ def process_input():
         'metric': METRICS_MAP[metric],
         'value': result
     }
-    with open('temp.json', 'w') as temp:
-        json.dump(output, temp)
+    write_to_file(output)
     return redirect(url_for('sl_metrics'))
 
 
-def read_metric_file():
+# POS-tagger part
+@app.route('/pos-tagger')
+def pos():
+    form = InputForm()
+    output = read_temp_file()
+    return render_template('pos.html',
+                           form=form,
+                           title='POS-tagger',
+                           legend='SMTH',
+                           output=output)
+
+
+@app.route('/api/handle-pos-input', methods=['POST'])
+def process_pos():
+    data = [request.form.get('text_pos').split()]
+
+    data_prepared = []
+    for sentences in data:
+        data_prepared.append([features(sentences, index) for index in range(len(sentences))])
+
+    predicted_pos = crf.predict(data_prepared)[0]
+
+    output = {
+        'sentence': data,
+        'pos': predicted_pos
+    }
+    write_to_file(output)
+
+    return redirect(url_for('pos'))
+
+
+# Utils
+def read_temp_file():
     data = None
 
     # TODO: refactor
@@ -62,6 +93,11 @@ def read_metric_file():
         remove('temp.json')
 
     return data
+
+
+def write_to_file(output):
+    with open('temp.json', 'w') as temp:
+        json.dump(output, temp)
 
 
 if __name__ == '__main__':
