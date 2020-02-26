b082bac23da0db184ba06d91779263dd2817f1d7
WhiteJaeger <logerk3@gmail.com>
2020-02-26 17:48:17 +0300

    move to app folder

--- app/app.py
+++ app/app.py
@@ -1,13 +1,9 @@
 import secrets
-import nltk
-import sklearn
-from sklearn_crfsuite import CRF
 # from NLP.pos import prepareData
 from flask import Flask, render_template, redirect, url_for, request, json
-from forms import InputForm
+from app.forms import InputForm
 from os import path, remove, getenv
-from waitress import serve
-from constants import METRICS_MAP, METRICS_FUNCTIONS
+from app.constants import METRICS_MAP, METRICS_FUNCTIONS
 from NLP.pos import features
 from joblib import load
 
@@ -43,7 +39,7 @@ def create_app():
     # )
     # crf.fit(X_train, y_train)
     # print('AAAAA')
-    crf = load('models/crfWJSModel90k.joblib')
+    crf = load('../models/crfWJSModel90k.joblib')
 
     app = Flask(__name__)
     app.secret_key = getenv('SECRET_KEY', secrets.token_urlsafe())
