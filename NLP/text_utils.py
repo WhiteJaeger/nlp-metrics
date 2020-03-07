f8c8e8323cac782dff6569e927dc65986b11110e
WhiteJaeger <logerk3@gmail.com>
2020-03-07 11:52:49 +0300

    some renaming

--- NLP/text_utils.py
+++ NLP/text_utils.py
@@ -39,8 +39,8 @@ def prepare_str(text: str, contraction_expansion=True, text_lower_case=False,
         text = [text.split()]
 
         prepared_text = []
-        for sentences in text:
-            prepared_text.append([encode_word(sentences, index) for index in range(len(sentences))])
+        for sentence in text:
+            prepared_text.append([encode_word(sentence, w_index) for w_index in range(len(sentence))])
         return prepared_text
 
     return text
@@ -102,44 +102,44 @@ def map_word_pos(prepared_sentence: str, pos_tags: list) -> list:
     return list(mapped)
 
 
-def encode_word(sentence: list, index: int) -> dict:
+def encode_word(sentence: list, word_index: int) -> dict:
     """
     Function to encode a single word in the given sentence.
 
     :param sentence: list of words: [word1, word2, word3 etc.]
     :type sentence: list
-    :param index: position of the word in the sentence
-    :type index: int
+    :param word_index: position of the word in the sentence
+    :type word_index: int
     :return: representation of a word as its features
     :rtype: dict
     """
 
     # If the given word is None or empty str -> return special tag
-    if not sentence[index]:
+    if not sentence[word_index]:
         return {
                 'Not-a-word': 1
         }
 
     return {
-            'is_first_capital':    int(sentence[index][0].isupper()),
-            'is_first_word':       int(index == 0),
-            'is_last_word':        int(index == len(sentence) - 1),
-            'is_complete_capital': int(sentence[index].upper() == sentence[index]),
+            'is_first_capital':    int(sentence[word_index][0].isupper()),
+            'is_first_word':       int(word_index == 0),
+            'is_last_word':        int(word_index == len(sentence) - 1),
+            'is_complete_capital': int(sentence[word_index].upper() == sentence[word_index]),
             # Consider the information in the prev word
-            'prev_word':           '' if index == 0 else sentence[index - 1],
+            'prev_word':           '' if word_index == 0 else sentence[word_index - 1],
             # Consider the information in the next word
-            'next_word':           '' if index == len(sentence) - 1 else sentence[index + 1],
-            'is_numeric':          int(sentence[index].isdigit()),
+            'next_word':           '' if word_index == len(sentence) - 1 else sentence[word_index + 1],
+            'is_numeric':          int(sentence[word_index].isdigit()),
             # For ABC123 cases
-            'is_alphanumeric':     int(bool((re.match('^(?=.*[0-9]$)(?=.*[a-zA-Z])', sentence[index])))),
+            'is_alphanumeric':     int(bool((re.match('^(?=.*[0-9]$)(?=.*[a-zA-Z])', sentence[word_index])))),
             # Extracting the morphological info of the given word
-            'prefix_1':            sentence[index][0],
-            'prefix_2':            sentence[index][:2],
-            'prefix_3':            sentence[index][:3],
-            'prefix_4':            sentence[index][:4],
-            'suffix_1':            sentence[index][-1],
-            'suffix_2':            sentence[index][-2:],
-            'suffix_3':            sentence[index][-3:],
-            'suffix_4':            sentence[index][-4:],
-            'word_has_hyphen':     1 if '-' in sentence[index] else 0
+            'prefix_1':            sentence[word_index][0],
+            'prefix_2':            sentence[word_index][:2],
+            'prefix_3':            sentence[word_index][:3],
+            'prefix_4':            sentence[word_index][:4],
+            'suffix_1':            sentence[word_index][-1],
+            'suffix_2':            sentence[word_index][-2:],
+            'suffix_3':            sentence[word_index][-3:],
+            'suffix_4':            sentence[word_index][-4:],
+            'word_has_hyphen':     1 if '-' in sentence[word_index] else 0
     }
