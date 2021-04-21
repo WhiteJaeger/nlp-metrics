cfaa16f57f510e6122b8f382c1d3d6050e4a272a
akashchi <andrey.kashchikhin@intel.com>
2021-04-21 11:56:30 +0300

    add tree representation, extractors

--- NLP/tree_constructor.py
+++ NLP/tree_constructor.py
@@ -1,49 +1,103 @@
+from typing import Any
+
 import spacy
 from spacy.tokens import Doc
 from spacy.tokens import Token
-from nltk import Tree
-from pprint import pprint
 
-from NLP.tree_transformer import tree_to_dict
-
-nlp = spacy.load('en_core_web_sm')
-# doc: Doc = nlp(u'i have a red pen')
+nlp: spacy.Language = spacy.load('en_core_web_sm')
 doc: Doc = nlp(u'Diplomatic staff would go home in a fifth plane')
-
-doc_tree = list(tree_to_dict(doc.to_json(), ['end', 'start', 'pos', 'dep', 'morph', 'lemma']))[0]
-pprint(doc_tree)
 print('*' * 100)
 
 
-def has_children(token_info: dict) -> bool:
-    constant_attrs = ['head', 'id', 'tag', 'text']
-
-    return len([key for key in token_info.keys() if key not in constant_attrs]) > 0
+def extract_1_length(parsed_doc: Doc) -> tuple[Any, ...]:
+    return tuple(token for token in parsed_doc)
 
 
-def extract_children(token_info: list) -> list:
-    constant_attrs = ['head', 'id', 'tag', 'text']
+def extract_2_length(parsed_doc: Doc) -> tuple[Any, ...]:
+    result = []
+    for token in parsed_doc:
+        if list(token.children):
+            result.append(token)
+    return tuple(result)
 
-    return [key for key in token_info if key not in constant_attrs]
 
-
-print(f'FROM TOP LEVEL: {extract_children(doc_tree)}')
-print(f'FROM ONE OF THE CHILDREN [prep]: {extract_children(doc_tree["prep"])}')
+print(f'2-LENGTH: {extract_2_length(doc)}')
+print(f'CHILDREN OF {extract_2_length(doc)[0]}: {list(extract_2_length(doc)[0].children)}')
+print(f'CHILDREN OF {extract_2_length(doc)[1]}: {list(extract_2_length(doc)[1].children)}')
 print('*' * 100)
 
 
-# a = list()
-# def myprint(d):
-#     for k, v in d.items():
-#         if isinstance(v, dict):
-#             a.append(v)
-#             myprint(v)
-#         else:
-#             # print("{0}: {1}".format(k, v))
-#             pass
+def extract_3_length(parsed_doc: Doc) -> tuple[Any, ...]:
+    result = []
+    two_length: tuple = extract_2_length(parsed_doc)
+    for token in two_length:
+        for child in token.children:
+            if list(child.children):
+                result.append(token)
+                break
+
+    return tuple(result)
 
 
-# myprint(doc_tree['prep'])
-# print(a)
+print(f'3-LENGTH: {extract_3_length(doc)}')
+print('*' * 100)
 
 print('*' * 100)
+
+
+class SyntaxTreeParser:
+    def __init__(self, head_token: Token):
+        self.head_token = head_token
+        self.children = self.__get_2_level_deep()
+        self.grand_children = self.__get_3_level_deep()
+        self.length = self.__determine_length()
+
+    def __determine_length(self) -> int:
+        if self.grand_children:
+            return 3
+        elif self.children:
+            return 2
+        else:
+            return 1
+
+    @property
+    def head(self):
+        return self.head_token
+
+    def __get_2_level_deep(self) -> tuple:
+        return tuple(self.head.children)
+
+    def __get_3_level_deep(self) -> tuple:
+        result = []
+        second_level = self.__get_2_level_deep()
+        if not second_level:
+            return ()
+
+        for token in second_level:
+            children = tuple(token.children)
+            if tuple(token.children):
+                for child in children:
+                    result.append(child)
+        return tuple(result)
+
+
+# a = SyntaxTreeParser(extract_2_length(doc)[1])
+# print(a.head)
+# print(a.length)
+# print(a.children)
+# print(a.grand_children)
+# print('*' * 100)
+#
+# a = SyntaxTreeParser(extract_3_length(doc)[1])
+# print(a.head)
+# print(a.length)
+# print(a.children)
+# print(a.grand_children)
+# print('*' * 100)
+#
+# a = SyntaxTreeParser(doc[0])
+# print(a.head)
+# print(a.length)
+# print(a.children)
+# print(a.grand_children)
+# print('*' * 100)
