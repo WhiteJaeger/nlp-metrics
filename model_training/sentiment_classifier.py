26a4e34cd7d739f3dbccf1da569f2431e0d7df6d
akashchi <andrey.kashchikhin@intel.com>
2021-05-03 09:59:02 +0300

    refactor, rename pipeline for sentiment analyzer

--- model_training/sentiment_classifier.py
+++ model_training/sentiment_classifier.py
@@ -10,15 +10,15 @@ from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
 
 
-def remove_noise(tweet_tokens, stop_words=()):
+def remove_noise(tokens_collection: list, stop_words: tuple = ()):
     cleaned_tokens = []
 
-    for token, tag in pos_tag(tweet_tokens):
+    for token, tag in pos_tag(tokens_collection):
         token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|' \
                        '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
         token = re.sub("(@[A-Za-z0-9_]+)", "", token)
 
-        if tag.startswith("NN"):
+        if tag.startswith('NN'):
             pos = 'n'
         elif tag.startswith('VB'):
             pos = 'v'
@@ -33,45 +33,40 @@ def remove_noise(tweet_tokens, stop_words=()):
     return cleaned_tokens
 
 
-def get_all_words(cleaned_tokens_list):
-    for tokens in cleaned_tokens_list:
-        for token in tokens:
+def get_all_words(cleaned_tokens: list):
+    for tokens_collection in cleaned_tokens:
+        for token in tokens_collection:
             yield token
 
 
-def get_tweets_for_model(cleaned_tokens_list):
-    for tweet_tokens in cleaned_tokens_list:
-        yield dict([token, True] for token in tweet_tokens)
+def get_tweets_for_model(cleaned_tokens: list):
+    for tokens_collection in cleaned_tokens:
+        yield dict([token, True] for token in tokens_collection)
 
 
 if __name__ == "__main__":
 
-    positive_tweets = twitter_samples.strings('positive_tweets.json')
-    negative_tweets = twitter_samples.strings('negative_tweets.json')
-    text = twitter_samples.strings('tweets.20150430-223406.json')
-    tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0]
-
-    stop_words = stopwords.words('english')
+    stop_words_eng = stopwords.words('english')
 
     positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
     negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
 
-    positive_cleaned_tokens_list = []
-    negative_cleaned_tokens_list = []
+    positive_cleaned_tokens = []
+    negative_cleaned_tokens = []
 
     for tokens in positive_tweet_tokens:
-        positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+        positive_cleaned_tokens.append(remove_noise(tokens, stop_words_eng))
 
     for tokens in negative_tweet_tokens:
-        negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+        negative_cleaned_tokens.append(remove_noise(tokens, stop_words_eng))
 
-    all_pos_words = get_all_words(positive_cleaned_tokens_list)
+    all_pos_words = get_all_words(positive_cleaned_tokens)
 
     freq_dist_pos = FreqDist(all_pos_words)
     print(freq_dist_pos.most_common(10))
 
-    positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
-    negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
+    positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens)
+    negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens)
 
     positive_dataset = [(tweet_dict, "Positive")
                         for tweet_dict in positive_tokens_for_model]
