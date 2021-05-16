1b16533322cbd4103044bfff8564c84629e992cd
akashchi <andrey.kashchikhin@intel.com>
2021-05-16 22:01:58 +0300

    prettify

--- main/models.py
+++ main/models.py
@@ -6,17 +6,18 @@ from joblib import load
 from nltk import NaiveBayesClassifier
 from sklearn.pipeline import Pipeline
 
+# Get path to the models folder
+APP_ROOT = Path(os.path.dirname(__file__)).parent
+PATH_TO_MODELS = os.path.join(APP_ROOT, 'models')
+
 # Load SpaCy model
 MODEL: spacy.Language = spacy.load('en_core_web_md')
 
-APP_ROOT = Path(os.path.dirname(__file__)).parent
-PATH_TO_MODELS_FOLDER = os.path.join(APP_ROOT, 'models')
-
 # Load pre-trained POS Tagger model
-POS_TAGGING = load(os.path.join(PATH_TO_MODELS_FOLDER, 'crfWJSModel.joblib'))
+POS_TAGGING = load(os.path.join(PATH_TO_MODELS, 'crfWJSModel.joblib'))
 
 # Load sentiment classifier
-SENTIMENT_CLASSIFIER: NaiveBayesClassifier = load(os.path.join(PATH_TO_MODELS_FOLDER, 'sentiment_classifier.joblib'))
+SENTIMENT_CLASSIFIER: NaiveBayesClassifier = load(os.path.join(PATH_TO_MODELS, 'sentiment_classifier.joblib'))
 
 # Load genre classifier
-GENRE_CLASSIFIER: Pipeline = load(os.path.join(PATH_TO_MODELS_FOLDER, 'genre_classifier.joblib'))
+GENRE_CLASSIFIER: Pipeline = load(os.path.join(PATH_TO_MODELS, 'genre_classifier.joblib'))
