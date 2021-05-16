1ff5403ca8f8d6a2c05deeecd059999b3c627e22
akashchi <andrey.kashchikhin@intel.com>
2021-05-16 21:55:31 +0300

    scale STM to 1, correct interface - more like NLTK

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import spacy
 from joblib import load
@@ -8,11 +9,14 @@ from sklearn.pipeline import Pipeline
 # Load SpaCy model
 MODEL: spacy.Language = spacy.load('en_core_web_md')
 
+APP_ROOT = Path(os.path.dirname(__file__)).parent
+PATH_TO_MODELS_FOLDER = os.path.join(APP_ROOT, 'models')
+
 # Load pre-trained POS Tagger model
-POS_TAGGING = load(os.path.join('models', 'crfWJSModel.joblib'))
+POS_TAGGING = load(os.path.join(PATH_TO_MODELS_FOLDER, 'crfWJSModel.joblib'))
 
 # Load sentiment classifier
-SENTIMENT_CLASSIFIER: NaiveBayesClassifier = load(os.path.join('models', 'sentiment_classifier.joblib'))
+SENTIMENT_CLASSIFIER: NaiveBayesClassifier = load(os.path.join(PATH_TO_MODELS_FOLDER, 'sentiment_classifier.joblib'))
 
 # Load genre classifier
-GENRE_CLASSIFIER: Pipeline = load(os.path.join('models', 'genre_classifier.joblib'))
+GENRE_CLASSIFIER: Pipeline = load(os.path.join(PATH_TO_MODELS_FOLDER, 'genre_classifier.joblib'))
